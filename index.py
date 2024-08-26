36d8d08a9f2668fb817654527453832ebe9f855c
sunatl <ibragimovusmon68@gmail.com>
2024-08-26 19:51:21 +0300

    1 task

---
+++ index.py
@@ -0,0 +1,18 @@
+
+def main(a):
+    if a == "Darth Vader":
+        print(f"Luke,I am your father")
+    elif a == "Leia":
+        print(f"Luke,I am your sister")
+    elif a == "Han":
+        print(f"Luke,I am your brother")
+    elif a == "R2D2":
+        print(f"Luke,I am your droid")
+main("Darth Vader")
+main("Leia")
+main("Han")
+main("R2D2")
+        
+        
+        
+        
