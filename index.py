a0e436fb151257e35b34b3e338949d755df3e76a
sunatl <ibragimovusmon68@gmail.com>
2024-08-27 11:55:00 +0300

    9 task

--- index.py
+++ index.py
@@ -1,18 +1,29 @@
 
+# def main(a):
+#     if a == "Darth Vader":
+#         print(f"Luke,I am your father")
+#     elif a == "Leia":
+#         print(f"Luke,I am your sister")
+#     elif a == "Han":
+#         print(f"Luke,I am your brother")
+#     elif a == "R2D2":
+#         print(f"Luke,I am your droid")
+# main("Darth Vader")
+# main("Leia")
+# main("Han")
+# main("R2D2")
+# main("Darth Vader")
+
+
+
 def main(a):
-    if a == "Darth Vader":
-        print(f"Luke,I am your father")
-    elif a == "Leia":
-        print(f"Luke,I am your sister")
-    elif a == "Han":
-        print(f"Luke,I am your brother")
-    elif a == "R2D2":
-        print(f"Luke,I am your droid")
-main("Darth Vader")
-main("Leia")
-main("Han")
-main("R2D2")
-        
+    for i in a[1:]:
+        print(i,end="")
+main("apple")
+print()
+main("cherry")
+print()
+main("plum")
         
         
         
