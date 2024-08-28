ea974378c3e6ab9d24c093972001920cc339d417
sunatl <ibragimovusmon68@gmail.com>
2024-08-28 11:32:13 +0300

    task 2

--- index1.py
+++ index1.py
@@ -1,10 +1,21 @@
-from datetime import datetime
-def main(date):
-    if date.month == 10 and date.day == 31:
-        return "Bonfire toffee"
-    else:
-        return "toffee"
-print(main(datetime.strptime("2013/10/31", "%Y/%m/%d")))  # "Bonfire toffee"
-print(main(datetime.strptime("2012/07/31", "%Y/%m/%d")))  # "toffee"
-print(main(datetime.strptime("2011/10/12", "%Y/%m/%d")))  # "toffee"
+# from datetime import datetime
+# def main(date):
+#     if date.month == 10 and date.day == 31:
+#         return "Bonfire toffee"
+#     else:
+#         return "toffee"
+# print(main(datetime.strptime("2013/10/31", "%Y/%m/%d")))
+# print(main(datetime.strptime("2012/07/31", "%Y/%m/%d")))
+# print(main(datetime.strptime("2011/10/12", "%Y/%m/%d")))
+
+
+
+
+
+def main(a):
+    return (a + 99) // 100
+print(main(2005))  # 21
+print(main(1950))  # 20
+print(main(1900))  # 19
+  
 
