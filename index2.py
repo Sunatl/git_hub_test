359ecffc9d00e9b87b81abe4450ca2bfc36fa84f
sunatl <ibragimovusmon68@gmail.com>
2024-08-29 12:57:23 +0300

    task 04

--- index2.py
+++ index2.py
@@ -14,20 +14,32 @@
 
 
 
+# import random
+# def main(a):
+#     l = 'abcdefghijklmnopqrstuvwxyz'
+#     u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     t = '0123456789'
+#     r = '!@#$%^&*()_+-=[]{}|;:,.<>?/'
+#     c = l + u + t + r
+#     password = ''.join(random.choice(c) for _ in range(a))
+#     return password
+# s = int(input("Enter the desired password length: "))
+# password = main(s)
+# print("Generated password:", password)
+
+
+
 import random
-def main(a):
-    l = 'abcdefghijklmnopqrstuvwxyz'
-    u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    t = '0123456789'
-    r = '!@#$%^&*()_+-=[]{}|;:,.<>?/'
-    c = l + u + t + r
-    password = ''.join(random.choice(c) for _ in range(a))
-    return password
-s = int(input("Enter the desired password length: "))
-password = main(s)
-print("Generated password:", password)
-
-
+s = ["Rock", "Paper", "Scissors"]
+c = int(input("Select your move (1 for Rock, 2 for Paper, 3 for Scissors): ")) - 1
+r = random.randint(0, 2)
+print("Computer chose", s[r],end="")
+if c == r:
+    print(".It's a tie!")
+elif (c == 0 and r == 2) or (c == 1 and r == 0) or (c == 2 and r == 1):
+    print(".You win!")
+else:
+    print(".You lose!")
 
 
 
