52adfb0dc8c6f97aabc16542e66c9062358d574e
sunatl <ibragimovusmon68@gmail.com>
2024-08-29 12:20:01 +0300

    task 03

--- index2.py
+++ index2.py
@@ -1,12 +1,35 @@
 
+# import random
+# a1 = [random.randint(0, 1) for _ in range(5)]
+# a2 = [random.randint(0, 1) for _ in range(5)]
+# arl = a1 == a2
+# print("First array:")
+# print(a1)
+# print("Second array:")
+# print(a2)
+# print("Test above two arrays are equal or not!")
+# print(arl)
+
+
+
+
+
 import random
-a1 = [random.randint(0, 1) for _ in range(5)]
-a2 = [random.randint(0, 1) for _ in range(5)]
-arl = a1 == a2
-print("First array:")
-print(a1)
-print("Second array:")
-print(a2)
-print("Test above two arrays are equal or not!")
-print(arl)
+def main(a):
+    l = 'abcdefghijklmnopqrstuvwxyz'
+    u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    t = '0123456789'
+    r = '!@#$%^&*()_+-=[]{}|;:,.<>?/'
+    c = l + u + t + r
+    password = ''.join(random.choice(c) for _ in range(a))
+    return password
+s = int(input("Enter the desired password length: "))
+password = main(s)
+print("Generated password:", password)
+
+
+
+
+
+
 
