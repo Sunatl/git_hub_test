a86e443cd0712c0ca0b9fdcdc3866f20f83ac7c3
sunatl <ibragimovusmon68@gmail.com>
2024-08-29 13:25:10 +0300

    task 01

--- index2.py
+++ index2.py
@@ -29,19 +29,49 @@
 
 
 
+# import random
+# s = ["Rock", "Paper", "Scissors"]
+# c = int(input("Select your move (1 for Rock, 2 for Paper, 3 for Scissors): ")) - 1
+# r = random.randint(0, 2)
+# print("Computer chose", s[r],end="")
+# if c == r:
+#     print(".It's a tie!")
+# elif (c == 0 and r == 2) or (c == 1 and r == 0) or (c == 2 and r == 1):
+#     print(".You win!")
+# else:
+#     print(".You lose!")
+
+
+
+
+
+
+
+
 import random
-s = ["Rock", "Paper", "Scissors"]
-c = int(input("Select your move (1 for Rock, 2 for Paper, 3 for Scissors): ")) - 1
-r = random.randint(0, 2)
-print("Computer chose", s[r],end="")
-if c == r:
-    print(".It's a tie!")
-elif (c == 0 and r == 2) or (c == 1 and r == 0) or (c == 2 and r == 1):
-    print(".You win!")
-else:
-    print(".You lose!")
+cnt = 0
+lives = 5
+r = random.randint(1, 100)
+print("Guess the number between 1 and 100. You have 5 lives.")
+while cnt < lives:
+        a = int(input("Enter your guess: "))
+        
+        if a < 1 or a > 100:
+            print("Please enter a number between 1 and 100.")
+            continue
+        cnt += 1
+        if a == r:
+            print("You win! The number was", r)
+            break
+        elif a < r:
+            print("Too low")
+        elif a > r:
+            print("Too high")
+        if cnt >= lives:
+            print("Game over! The number was", r)
+            break
 
-
-
-
+        
+        
+    
 
